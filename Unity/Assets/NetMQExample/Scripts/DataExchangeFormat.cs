b4717130d1d1811f3f0d5e95fb69bf7f48ff104d
Anke <areinsch@uni-bremen.de>
2020-01-20 14:29:48 +0000

    Added DataExchangeFormat Script

---
+++ Unity/Assets/NetMQExample/Scripts/DataExchangeFormat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataExchangeFormat
+{
+  public string identifier; // DICOM Datensatz
+
+  // Headset
+  // Position x,y,z
+  // Rotation -> wie angegeben in Slicer?
+
+  // Controller1
+  // Position x,y,z
+  // Rotation -> wie angegeben in Slicer?
+
+  // Controller2
+  // Position x,y,z
+  // Rotation -> wie angegeben in Slicer?
+
+  // Bildnummer
+
+  // Kontrast (ggf. nach pull)
+
+
+}
