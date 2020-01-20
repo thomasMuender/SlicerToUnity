65446f22cc1a45fdb60d8d520dd02cb478911103
Thomas Muender <thom@tzi.de>
2020-01-20 11:02:15 +0000

    Initial project files

---
+++ Unity/Assets/NetMQExample/Scripts/HelloClient.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+
+public class HelloClient : MonoBehaviour
+{
+    private HelloRequester _helloRequester;
+
+    private void Start()
+    {
+        _helloRequester = new HelloRequester();
+        _helloRequester.Start();
+    }
+
+    private void Update()
+    {
+ 
+    }
+
+    private void OnDestroy()
+    {
+        _helloRequester.Stop();
+    }
+}
---
+++ Unity/Assets/NetMQExample/Scripts/HelloRequester.cs
@@ -0,0 +1,44 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using UnityEngine;
+
+/// <summary>
+///     Example of requester who only sends Hello. Very nice guy.
+///     You can copy this class and modify Run() to suits your needs.
+///     To use this class, you just instantiate, call Start() when you want to start and Stop() when you want to stop.
+/// </summary>
+public class HelloRequester : RunAbleThread
+{
+
+    protected override void Run()
+    {
+        AsyncIO.ForceDotNet.Force(); // this line is needed to prevent unity freeze after one use, not sure why yet
+        using (SubscriberSocket client = new SubscriberSocket())
+        {
+            client.Connect("tcp://localhost:5556");
+            client.Subscribe("");
+            Debug.Log("Connected");
+            while (Running)
+            {
+                    string message = null;
+                    bool gotMessage = false;
+                    while (Running)
+                    {
+                        gotMessage = client.TryReceiveFrameString(out message); // this returns true if it's successful
+                        if (gotMessage) break;
+                    }
+
+                    if (gotMessage) {
+                        Debug.Log(message);
+                    }
+            }
+            client.Close();
+        }
+        NetMQConfig.Cleanup();
+    }
+
+    protected override void Clean()
+    {
+
+    }
+}
