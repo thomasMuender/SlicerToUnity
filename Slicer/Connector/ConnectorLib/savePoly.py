411226911e4198f15cfc61cbf302d7cc35574724
Anke <areinsch@uni-bremen.de>
2020-01-22 17:06:16 +0000

    SLICER: shortend savePoly code

--- Slicer/Connector/ConnectorLib/savePoly.py
+++ Slicer/Connector/ConnectorLib/savePoly.py
@@ -1,23 +1,38 @@
+import numpy as np
+from slicer.app import *
+
 def savePoly():
 
-    nodes = slicer.util.getNodesByClass('vtkMRMLSegmentationNode') # Segmentation _nodes_
-    s = []  # array of segmentations
-    sno = []  # array of segmentation _segments_
+    segtion = []  # array of segmentations
+    segment = []  # array of segmentation _segments_
     polys = [] # _polyData_ of segments
-    for i in range(len(nodes)):
-        s.append(nodes[i].GetSegmentation())
 
-    for node in s:
+    segnodes = slicer.util.getNodesByClass('vtkMRMLSegmentationNode')  # Segmentation _nodes_
+
+    for i in range(len(segnodes)):
+        segtion.append(segnodes[i].GetSegmentation())
+        if segnodes[i].GetSegmentation().GetNumberOfSegments() > 0:
+            for j in range(node.GetNumberOfSegments()):
+                polys.append(segnodes[i].GetClosedSurfaceRepresentation(node.GetNthSegmentID(j)))
+
+
+### longer version of code above
+
+    '''
+    for i in range(len(segnodes)):
+        segtion.append(segnodes[i].GetSegmentation())
+    
+    for node in segtion:
         if node.GetNumberOfSegments() > 0:
             for i in range(node.GetNumberOfSegments()):
-                sno.append(node.GetNthSegmentID(i))
+                segment.append(node.GetNthSegmentID(i))
+    
+    for i in range(len(segnodes)):
+        if segnodes[i].GetSegmentation().GetNumberOfSegments() > 0:
+            for poly in range(len(segment)):
+                polys.append(segnodes[i].GetClosedSurfaceRepresentation(segment[poly]))'''
 
-    for i in range(len(nodes)):
-        if nodes[i].GetSegmentation().GetNumberOfSegments() > 0:
-            for poly in range(len(sno)):
-                polys.append(nodes[i].GetClosedSurfaceRepresentation(sno[poly]))
-
-#####
+##### from slicer testing
 
 ''' slicer.util.getNodesByClass('vtkMRMLSegmentationNode')
 n = getNode("Segmentation_1")
