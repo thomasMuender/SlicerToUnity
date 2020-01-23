ae48d8eabef9044483ce2a40d443a8a0166f3736
Thomas Mildner <thomas.s.mildner@gmail.com>
2020-01-23 10:00:28 +0000

    SLICER: worked on connectionLib functionality

--- Slicer/Connector/ConnectorLib/DataSelector.py
+++ Slicer/Connector/ConnectorLib/DataSelector.py
@@ -35,3 +35,139 @@ class DataSelector:
         a = np.array(compositionNodesIDs[0])
         return a
 
+
+    def getROI(self):
+        print 'not working'
+
+
+    def getRuler(self):
+        print 'not working'
+
+
+
+class Annotation:
+
+    def __init__(self):
+        print 'Initalized new Annotation'
+
+    type = 0
+    isRuler = False
+    isROI = False
+    isFiducial = False
+
+    def setType(self, className):
+        if className == 'vtkMRMLAnnotationRulerNode':
+            self.isRuler = True
+            self.type = 0
+        elif className == 'vtkMRMLAnnotationROINode':
+            self.isROI = True
+            self.type = 1
+        elif className == 'vtkMRMLMarkupsFiducialNode':
+            self.isFiducial = True
+            self.type = 2
+
+    def getType(self):
+        return self.type
+
+
+def getAllAnnotations():
+    allRulers = slicer.mrmlScene.GetNodesByClass('vtkMRMLAnnotationRulerNode')
+    allROIs = slicer.mrmlScene.GetNodesByClass('vtkMRMLAnnotationROINode')
+    allFiducials = slicer.mrmlScene.GetNodesByClass('vtkMRMLMarkupsFiducialNode')
+
+    
+
+
+
+
+
+
+def getAnnotations():
+    listNodeID = 'vtkMRMLAnnotationHierarchyNode1'  # All Annotations List
+    annotationHierarchyNode = slicer.mrmlScene.GetNodeByID(listNodeID)
+    print annotationHierarchyNode
+    annotationIDs = []
+    annotationListIDs = []
+
+    i = annotationHierarchyNode.GetNumberOfChildrenNodes()
+
+    while i >= 0:
+        print i
+        innerList = annotationHierarchyNode.GetNthChildNode(i)
+        if innerList is None:
+            i -= 1
+            continue
+        annotationListIDs.append(innerList)
+        print 'added ' + str(innerList.GetID()) + ' to annotationListIDs'
+        j = innerList.GetNumberOfChildrenNodes() - 1
+        i -= 1
+        while j >= 0:
+            print innerList.GetNthChildNode(j)
+            if innerList.GetNthChildNode(j) is None:
+                j -= 1
+                continue
+            if innerList.GetNthChildNode(j).GetAssociatedNode() is None:
+                j -= 1
+                continue
+            annotationIDs.append(innerList.GetNthChildNode(j).GetAssociatedNode())
+            print 'added ' + str(innerList.GetNthChildNode(j).GetAssociatedNode().GetID()) + ' to annotationIDs'
+            j -= 1
+
+
+def getROIControlPoints( attrID ):
+    attribute = slicer.mrmlScene.GetNodeByID(attrID)
+    p1 = [0,0,0] # origin
+    p2 = [0,0,0] # radius
+    attribute.GetRadiusXYZ(p1)
+    attribute.GetXYZ(p2)
+    controlPoints = [p1, p2]
+    return controlPoints
+
+
+def getRulerPositions( attrID ):
+    attribute = slicer.mrmlScene.GetNodeByID(attrID)
+    p1 = [0,0,0] # start point
+    p2 = [0,0,0] # end point
+    attribute.GetPosition1(p1)
+    attribute.GetPosition2(p2)
+    positionPoints = [p1, p2]
+    return positionPoints
+
+
+listNodeID = 'vtkMRMLAnnotationHierarchyNode1' # All Annotations List
+annotationHierarchyNode = slicer.mrmlScene.GetNodeByID(listNodeID)
+print annotationHierarchyNode
+annotationIDs = []
+annotationListIDs = []
+
+i = annotationHierarchyNode.GetNumberOfChildrenNodes()
+
+while i >= 0 :
+    print i
+    innerList = annotationHierarchyNode.GetNthChildNode(i)
+    if innerList is None:
+        i -= 1
+        continue
+    annotationListIDs.append(innerList)
+    print 'added ' + str(innerList.GetID()) + ' to annotationListIDs'
+    j = innerList.GetNumberOfChildrenNodes() - 1
+    i -= 1
+    while j >= 0:
+        print innerList.GetNthChildNode(j)
+        if innerList.GetNthChildNode(j) is None:
+            j -= 1
+            continue
+        if innerList.GetNthChildNode(j).GetAssociatedNode() is None:
+            j -= 1
+            continue
+        annotationIDs.append(innerList.GetNthChildNode(j).GetAssociatedNode())
+        print 'added ' + str(innerList.GetNthChildNode(j).GetAssociatedNode().GetID()) + ' to annotationIDs'
+        j -= 1
+
+
+
+i = annotationHierarchyNode.GetNumberOfChildrenNodes()
+while i >= 0 :
+    print i
+    i -= 1
+
--- Slicer/Connector/ConnectorLib/ThreeDVisualizor.py
+++ Slicer/Connector/ConnectorLib/ThreeDVisualizor.py
@@ -1,4 +1,47 @@
 import numpy as np
+import math
 from slicer.ScriptedLoadableModule import *
 
-class ThreeDVisualizor
+
+class ThreeDVisualizor:
+
+    def __init__(self):
+        print 'ThreeDVisualizor loaded.'
+
+
+    def viewPoint(self):
+        return
+
+
+    def drawSphere(self, x, y, z, w):
+        centerPointCoord = [x, y, z]
+        radius = 10
+        sphere = vtk.vtkSphereSource()
+        sphere.SetCenter(centerPointCoord)
+        sphere.SetRadius(radius)
+
+        modelsLogic = slicer.modules.models.logic()
+        model = modelsLogic.AddModel(sphere.GetOutput())
+        model.GetDisplayNode().SetSliceIntersectionVisibility(True)
+        model.GetDisplayNode().SetSliceIntersectionThickness(3)
+        model.GetDisplayNode().SetColor(1, 1, 0)
+
+centerPointCoord = [0, 0, 0]
+radius = 10
+sphere = vtk.vtkSphereSource()
+sphere.SetCenter(centerPointCoord)
+sphere.SetRadius(radius)
+
+modelsLogic = slicer.modules.models.logic()
+model = modelsLogic.AddModel(sphere.GetOutput())
+model.GetDisplayNode().SetSliceIntersectionVisibility(True)
+model.GetDisplayNode().SetSliceIntersectionThickness(3)
+model.GetDisplayNode().SetColor(1, 1, 0)
+
+
+
+
+viewPoint = slicer.app.layoutManager().threeDWidget(0).threeDView()
+
+
+
