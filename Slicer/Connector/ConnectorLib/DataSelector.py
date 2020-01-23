41b16b68912be41d3c5dca8780c9ef5418a9d153
Thomas Mildner <thomas.s.mildner@gmail.com>
2020-01-23 11:33:48 +0000

    SLICER: added annotation class and getAllAnnotations function

--- Slicer/Connector/ConnectorLib/DataSelector.py
+++ Slicer/Connector/ConnectorLib/DataSelector.py
@@ -1,12 +1,10 @@
 import numpy as np
 from slicer.app import *
 
-class DataSelector:
+class ActiveVolumeelector:
 
     def __init__(self):
         print('DataSelector active')
-
-
 
     def getActiveVolumeAsArray(_self):
         layoutManager = slicer.app.layoutManager()
@@ -20,7 +18,6 @@ class DataSelector:
 
         a = array(compositionNodesIDs[0])
         return a
-
 
     def getActiveVolumeAsNumpyArray(_self):
         layoutManager = slicer.app.layoutManager()
@@ -44,130 +41,189 @@ class DataSelector:
         print 'not working'
 
 
-
 class Annotation:
-
-    def __init__(self):
-        print 'Initalized new Annotation'
 
     type = 0
     isRuler = False
     isROI = False
     isFiducial = False
+    controlPoints = []
+    className = ''
+    id = ''
 
-    def setType(self, className):
-        if className == 'vtkMRMLAnnotationRulerNode':
+    def __init__(self, attrID):
+        self.id =  attrID
+        self.className = slicer.mrmlScene.GetNodeByID(attrID).GetClassName()
+        if attrID is '':
+            print 'No Attribute ID has been given.'
+            pass
+
+        if self.className == 'vtkMRMLAnnotationRulerNode':
             self.isRuler = True
             self.type = 0
-        elif className == 'vtkMRMLAnnotationROINode':
+            self._setRulerPositions()
+            print 'Set Annotation Type: RULER'
+        elif self.className == 'vtkMRMLAnnotationROINode':
             self.isROI = True
             self.type = 1
-        elif className == 'vtkMRMLMarkupsFiducialNode':
+            self._setROIControllPoints()
+            print 'Set Annotation Type: ROI'
+        elif self.className == 'vtkMRMLMarkupsFiducialNode':
             self.isFiducial = True
             self.type = 2
+            self._setFiducialPosition()
+            print 'Set Annotation Type: FIDUCIAL'
+
+    def _setROIControllPoints(self):
+        attribute = slicer.mrmlScene.GetNodeByID(self.id)
+        p1 = [0, 0, 0]  # origin
+        p2 = [0, 0, 0]  # radius
+        attribute.GetRadiusXYZ(p1)
+        attribute.GetXYZ(p2)
+        self.controlPoints = [p1, p2]
+
+    def _setRulerPositions(self):
+        attribute = slicer.mrmlScene.GetNodeByID(self.id)
+        p1 = [0, 0, 0]  # start point
+        p2 = [0, 0, 0]  # end point
+        attribute.GetPosition1(p1)
+        attribute.GetPosition2(p2)
+        self.controlPoints = [p1, p2]
+
+    def _setFiducialPosition(self):
+        attribute = slicer.mrmlScene.GetNodeByID(self.id)
+        p1 = [0,0,0]    # single fiducial point
+        count = attribute.GetNumberOfFiducials()
+        while count >= 0:
+            attribute.GetNthFiducialPosition(0,p1)
+            self.controlPoints.append(p1)
+            count -= 1
+
+    def getControlPoints(self):
+        return self.controlPoints
 
     def getType(self):
         return self.type
 
 
+
+
+
 def getAllAnnotations():
-    allRulers = slicer.mrmlScene.GetNodesByClass('vtkMRMLAnnotationRulerNode')
-    allROIs = slicer.mrmlScene.GetNodesByClass('vtkMRMLAnnotationROINode')
-    allFiducials = slicer.mrmlScene.GetNodesByClass('vtkMRMLMarkupsFiducialNode')
+    allAnnotations = []
+    rulerCollection = slicer.util.getNodesByClass('vtkMRMLAnnotationRulerNode')
+    roiCollection = slicer.util.getNodesByClass('vtkMRMLAnnotationROINode')
+    fiducialCollection = slicer.util.getNodesByClass('vtkMRMLMarkupsFiducialNode')
 
-    
+    rulerIDs = []
+    for ruler in rulerCollection:
+        rulerIDs.append(ruler.GetID())
 
+    roiIDs = []
+    for roi in roiCollection:
+        roiIDs.append(roi.GetID())
 
+    fiducialIDs = []
+    for fid in fiducialCollection:
+        fiducialIDs.append(fid.GetID())
 
+    for id in rulerIDs:
+        allAnnotations.append(Annotation(id))
 
-
-
-def getAnnotations():
-    listNodeID = 'vtkMRMLAnnotationHierarchyNode1'  # All Annotations List
-    annotationHierarchyNode = slicer.mrmlScene.GetNodeByID(listNodeID)
-    print annotationHierarchyNode
-    annotationIDs = []
-    annotationListIDs = []
-
-    i = annotationHierarchyNode.GetNumberOfChildrenNodes()
-
-    while i >= 0:
-        print i
-        innerList = annotationHierarchyNode.GetNthChildNode(i)
-        if innerList is None:
-            i -= 1
-            continue
-        annotationListIDs.append(innerList)
-        print 'added ' + str(innerList.GetID()) + ' to annotationListIDs'
-        j = innerList.GetNumberOfChildrenNodes() - 1
-        i -= 1
-        while j >= 0:
-            print innerList.GetNthChildNode(j)
-            if innerList.GetNthChildNode(j) is None:
-                j -= 1
-                continue
-            if innerList.GetNthChildNode(j).GetAssociatedNode() is None:
-                j -= 1
-                continue
-            annotationIDs.append(innerList.GetNthChildNode(j).GetAssociatedNode())
-            print 'added ' + str(innerList.GetNthChildNode(j).GetAssociatedNode().GetID()) + ' to annotationIDs'
-            j -= 1
-
-
-def getROIControlPoints( attrID ):
-    attribute = slicer.mrmlScene.GetNodeByID(attrID)
-    p1 = [0,0,0] # origin
-    p2 = [0,0,0] # radius
-    attribute.GetRadiusXYZ(p1)
-    attribute.GetXYZ(p2)
-    controlPoints = [p1, p2]
-    return controlPoints
-
-
-def getRulerPositions( attrID ):
-    attribute = slicer.mrmlScene.GetNodeByID(attrID)
-    p1 = [0,0,0] # start point
-    p2 = [0,0,0] # end point
-    attribute.GetPosition1(p1)
-    attribute.GetPosition2(p2)
-    positionPoints = [p1, p2]
-    return positionPoints
-
-
-listNodeID = 'vtkMRMLAnnotationHierarchyNode1' # All Annotations List
-annotationHierarchyNode = slicer.mrmlScene.GetNodeByID(listNodeID)
-print annotationHierarchyNode
-annotationIDs = []
-annotationListIDs = []
-
-i = annotationHierarchyNode.GetNumberOfChildrenNodes()
-
-while i >= 0 :
-    print i
-    innerList = annotationHierarchyNode.GetNthChildNode(i)
-    if innerList is None:
-        i -= 1
-        continue
-    annotationListIDs.append(innerList)
-    print 'added ' + str(innerList.GetID()) + ' to annotationListIDs'
-    j = innerList.GetNumberOfChildrenNodes() - 1
-    i -= 1
-    while j >= 0:
-        print innerList.GetNthChildNode(j)
-        if innerList.GetNthChildNode(j) is None:
-            j -= 1
-            continue
-        if innerList.GetNthChildNode(j).GetAssociatedNode() is None:
-            j -= 1
-            continue
-        annotationIDs.append(innerList.GetNthChildNode(j).GetAssociatedNode())
-        print 'added ' + str(innerList.GetNthChildNode(j).GetAssociatedNode().GetID()) + ' to annotationIDs'
-        j -= 1
+    return allAnnotations
 
 
 
-i = annotationHierarchyNode.GetNumberOfChildrenNodes()
-while i >= 0 :
-    print i
-    i -= 1
-
+#
+#
+#
+#
+# def getAnnotations():
+#     listNodeID = 'vtkMRMLAnnotationHierarchyNode1'  # All Annotations List
+#     annotationHierarchyNode = slicer.mrmlScene.GetNodeByID(listNodeID)
+#     print annotationHierarchyNode
+#     annotationIDs = []
+#     annotationListIDs = []
+#
+#     i = annotationHierarchyNode.GetNumberOfChildrenNodes()
+#
+#     while i >= 0:
+#         print i
+#         innerList = annotationHierarchyNode.GetNthChildNode(i)
+#         if innerList is None:
+#             i -= 1
+#             continue
+#         annotationListIDs.append(innerList)
+#         print 'added ' + str(innerList.GetID()) + ' to annotationListIDs'
+#         j = innerList.GetNumberOfChildrenNodes() - 1
+#         i -= 1
+#         while j >= 0:
+#             print innerList.GetNthChildNode(j)
+#             if innerList.GetNthChildNode(j) is None:
+#                 j -= 1
+#                 continue
+#             if innerList.GetNthChildNode(j).GetAssociatedNode() is None:
+#                 j -= 1
+#                 continue
+#             annotationIDs.append(innerList.GetNthChildNode(j).GetAssociatedNode())
+#             print 'added ' + str(innerList.GetNthChildNode(j).GetAssociatedNode().GetID()) + ' to annotationIDs'
+#             j -= 1
+#
+#
+# def getROIControlPoints( attrID ):
+#     attribute = slicer.mrmlScene.GetNodeByID(attrID)
+#     p1 = [0,0,0] # origin
+#     p2 = [0,0,0] # radius
+#     attribute.GetRadiusXYZ(p1)
+#     attribute.GetXYZ(p2)
+#     controlPoints = [p1, p2]
+#     return controlPoints
+#
+#
+# def getRulerPositions( attrID ):
+#     attribute = slicer.mrmlScene.GetNodeByID(attrID)
+#     p1 = [0,0,0] # start point
+#     p2 = [0,0,0] # end point
+#     attribute.GetPosition1(p1)
+#     attribute.GetPosition2(p2)
+#     positionPoints = [p1, p2]
+#     return positionPoints
+#
+#
+# listNodeID = 'vtkMRMLAnnotationHierarchyNode1' # All Annotations List
+# annotationHierarchyNode = slicer.mrmlScene.GetNodeByID(listNodeID)
+# print annotationHierarchyNode
+# annotationIDs = []
+# annotationListIDs = []
+#
+# i = annotationHierarchyNode.GetNumberOfChildrenNodes()
+#
+# while i >= 0 :
+#     print i
+#     innerList = annotationHierarchyNode.GetNthChildNode(i)
+#     if innerList is None:
+#         i -= 1
+#         continue
+#     annotationListIDs.append(innerList)
+#     print 'added ' + str(innerList.GetID()) + ' to annotationListIDs'
+#     j = innerList.GetNumberOfChildrenNodes() - 1
+#     i -= 1
+#     while j >= 0:
+#         print innerList.GetNthChildNode(j)
+#         if innerList.GetNthChildNode(j) is None:
+#             j -= 1
+#             continue
+#         if innerList.GetNthChildNode(j).GetAssociatedNode() is None:
+#             j -= 1
+#             continue
+#         annotationIDs.append(innerList.GetNthChildNode(j).GetAssociatedNode())
+#         print 'added ' + str(innerList.GetNthChildNode(j).GetAssociatedNode().GetID()) + ' to annotationIDs'
+#         j -= 1
+#
+#
+#
+# i = annotationHierarchyNode.GetNumberOfChildrenNodes()
+# while i >= 0 :
+#     print i
+#     i -= 1
+#
